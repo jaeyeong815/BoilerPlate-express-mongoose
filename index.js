ff47f03c59b56ff6a16b74abd8b8d614052fae7d
jaeyeong815 <jae0ee@naver.com>
2023-02-25 00:23:22 +0900

    feat: mongoDB, mongoose 연결

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+require('dotenv/config');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB mongoose Connected...'))
+  .catch((err) => console.log(err));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
